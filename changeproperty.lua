eb533c2a2a36bdb63f277d24ad67789c79387dae
presssssure <33643911+PressurizedSphere@users.noreply.github.com>
2019-11-07 13:54:15 -0500

    write property changing functions and comment old code

--- changeproperty.lua
+++ changeproperty.lua
@@ -1,5 +1,50 @@
+-- changes properties of the plugin and updates settings and guis accordingly
+
 local ChangeProperty = {}
 
+local Gui = require(script.Parent.gui)
+local HatUpdate = require(script.Parent.hatupdate)
+local Settings = require(script.Parent.settings)
+
+function ChangeProperty.ChangeHat(hatId)
+    -- if success returns true and hat is non nil, the id corresponds to a hat. if it doesn't, set the id back to the previous hat
+    local success, hat = pcall(function()
+        local hat = InsertService:LoadAsset(tonumber(hatId)):FindFirstChildOfClass("Accessory")
+        return hat
+    end)
+
+    if success and hat then
+        Settings.SetHatId(hatId)
+        HatUpdate.UpdateHat(hat)
+    else
+        Gui.HatId:SetValue(Settings.GetHatId())
+    end
+end
+
+function ChangeProperty.ChangeTransparency(visible)
+    local transparency = visible and 0 or 1
+    Settings.SetTransparency(transparency)
+end
+
+function ChangeProperty.ChangeHeight(height)
+	if tonumber(h) then
+        Settings.SetHeight(height)
+    else
+        Gui.Height:SetValue(Settings.GetHeight())
+	end
+end
+
+function ChangeProperty.ChangeEnabled(state)
+    Settings.SetEnabled(state)
+    
+	if state then
+        HatUpdate.Connect()
+    else
+        HatUpdate.Disconnect()
+	end
+end
+
+--[=[
 local function ChangeHat()
 	--[
 	local HatID = TextboxHatID:GetValue()
@@ -45,5 +90,6 @@ local function ChangeTransparency(visible)
 	local transparency = visible and 0 or 1
 	plugin:SetSetting("Transparency",transparency)
 end
+--]=]
 
 return ChangeProperty
