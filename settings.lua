986b095d2f2359a55ee0f425b5bd1f7054942ff7
presssssure <33643911+PressurizedSphere@users.noreply.github.com>
2019-11-07 14:04:01 -0500

    set up settings.lua with function headers

--- settings.lua
+++ settings.lua
@@ -1,9 +1,53 @@
+-- manages settings and stores values of settings
+local Settings = {}
+
+local hatId
+local height
+local transparency
+local enabled
+
 -- checks if any settings aren't nil, if all are nil (user has never used the plugin) it enables the plugin and sets default settings
-if not (plugin:GetSetting("Height") or plugin:GetSetting("Transparency") or plugin:GetSetting("HatID")) then
-	plugin:SetSetting("Height",5)
-	plugin:SetSetting("Transparency",0)
-	plugin:SetSetting("HatID",1028826)
-	plugin:SetSetting("Enabled",true)
+function Settings.CheckForFirstTimeUse()
+    if not (plugin:GetSetting("Height") or plugin:GetSetting("Transparency") or plugin:GetSetting("HatID")) then
+        plugin:SetSetting("HatID", 1028826)
+        plugin:SetSetting("Height", 5)
+        plugin:SetSetting("Transparency", 0)
+        plugin:SetSetting("Enabled", true)
+    end
 end
+
+function Settings.GetHatId()
+
+end
+
+function Settings.GetHeight()
+
+end
+
+function Settings.GetTransparent()
+
+end
+
+function Settings.GetEnabled()
+
+end
+
+function Settings.SetHatId()
+
+end
+
+function Settings.SetHeight()
+
+end
+
+function Settings.SetTransparent()
+
+end
+
+function Settings.SetEnabled()
+
+end
+
+
 
 return 0
