b046b60c90d42646a13cbdbc0d219e597f9cf9e8
presssssure <33643911+PressurizedSphere@users.noreply.github.com>
2019-11-08 08:10:21 -0500

    add gui to main

--- main.lua
+++ main.lua
@@ -1,6 +1,10 @@
+-- main script, starts all modules
+
 local Prechecks = require(script.prechecks)
 
 if not Prechecks.shouldRun() then script.Disabled = true end
 
 local Settings = require(script.settings)
-Settings.CheckForFirstTimeUseAndSetUp()-- checks for new users and sets default settings
+Settings.CheckForFirstTimeUseAndSetUp() -- checks for new users and sets default settings
+
+local Guis = require(script.gui) -- sets up and runs guis
