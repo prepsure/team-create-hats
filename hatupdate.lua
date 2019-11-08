923aac8708a279bc8529f09a6bce4b7817cd85a8
presssssure <33643911+PressurizedSphere@users.noreply.github.com>
2019-11-08 14:19:49 -0500

    Update hatupdate.lua

--- hatupdate.lua
+++ hatupdate.lua
@@ -1,17 +1,37 @@
 local HatUpdate = {}
 
 HatUpdate._connection
+HatUpdate.CurrentHat
+
+local function moveHat()
+    CurrentHat:FindFirstChildOfClass("Part").CFrame = workspace.CurrentCamera.CFrame + Vector3.new(0, Settings.GetHeight(), 0)
+end
 
 function HatUpdate.UpdateHat(hat)
-    
+    if HatUpdate.CurrentHat then
+        HatUpdate.CurrentHat:Destroy()
+    end
+
+    CurrentHat = hat
+    CurrentHat.Archivable = false
+    CurrentHat.Handle.Locked = true
+    CurrentHat.Parent = Folder
+
+    local handle = hat:FindFirstChildOfClass("Part")
+    handle.LocalTransparencyModifier = Settings.GetTransparency()
+
+    local attachment = handle:FindFirstChildOfClass("Attachment")
+    if attachment then
+        attachment:Destroy()
+    end
 end
 
 function HatUpdate.Connect()
-
+    HatUpdate._connection = RunService.RenderStepped:Connect(moveHat)
 end
 
 function HatUpdate.Disconnect()
-
+    HatUpdate._connection:Disconnect()
 end
 
 return HatUpdate
