958d6316afb92cf3021a081fdf4fca6f705be6ca
presssssure <33643911+PressurizedSphere@users.noreply.github.com>
2019-11-08 08:14:28 -0500

    setup hatupdate.lua

---
+++ hatupdate.lua
@@ -0,0 +1,17 @@
+local HatUpdate = {}
+
+HatUpdate._connection
+
+function HatUpdate.UpdateHat(hat)
+    
+end
+
+function HatUpdate.Connect()
+
+end
+
+function HatUpdate.Disconnect()
+
+end
+
+return HatUpdate
