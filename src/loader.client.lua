5536df5930bca5297ff3b514b8fdd71475769df6
paige <33643911+presssssure@users.noreply.github.com>
2021-02-25 11:35:02 -0500

    index through world

--- src/loader.client.lua
+++ src/loader.client.lua
@@ -14,6 +14,7 @@ end
 ----------
 
 -- cleanup from last time
+
 local CollectionService = game:GetService("CollectionService")
 local yourHatTag = game:GetService("Players").LocalPlayer.Name .. "'s hats"
 
@@ -23,10 +24,10 @@ end
 
 ----------
 
-local HatImporter = require(root.hat.importer)
-local Hat = require(root.hat)
-local Storage = require(root.storage)
-local PersistentInstance = require(root.persistentInstance)
+local HatImporter = require(root.hat.world.importer)
+local Hat = require(root.world.hat)
+local Storage = require(root.world.storage)
+local PersistentInstance = require(root.world.persistentInstance)
 
 local folder = Storage("SunRaysEffect", game:GetService("Players").LocalPlayer.Name)
 CollectionService:AddTag(folder, yourHatTag)
@@ -34,13 +35,12 @@ local persistentFolder = PersistentInstance.new(folder, workspace)
 
 local myId = 553870650
 local statusCode, accessory = HatImporter:LoadHat(myId)
-
 local persistentAccessory = PersistentInstance.new(accessory, persistentFolder)
 
 local myHat = Hat.new(persistentAccessory, {
     id = myId,
     offset = Vector3.new(0,5,0),
     scale = accessory.Handle.Mesh.Scale,
-    transformPriority = "Translate",
+    transformPriority = "rotate",
     visibleLocally = true,
 })
