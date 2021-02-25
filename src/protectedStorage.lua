9b4b2c02496c04a54ac73ad76313f98f193a3aaf
paige <33643911+presssssure@users.noreply.github.com>
2021-02-24 22:10:51 -0500

    add deletion protection

--- src/protectedStorage.lua
+++ src/protectedStorage.lua
@@ -1,4 +1,4 @@
-local RunService
+local RunService = game:GetService("RunService")
 
 local coolIconClass = "SunRaysEffect"
 local userName = game:GetService("Players").LocalPlayer.Name
@@ -9,44 +9,86 @@ local ProtectedStorage = {}
 ProtectedStorage.__index = ProtectedStorage
 
 
-function ProtectedStorage.new(parent)
-    local self = setmetatable({}, ProtectedStorage)
-
+local function makeFolder()
     local folder = Instance.new(coolIconClass)
     folder.Parent = workspace
     folder.Name = userName .. "'s hats"
 
-    self.folder = folder
+    return folder
+end
+
+
+
+function ProtectedStorage.new(parent)
+    local self = setmetatable({}, ProtectedStorage)
+
+    self.folder = makeFolder()
     self.parent = parent
 
-    self.items = {}
-    self._putBackFolder = folder.AncestryChanged:Connect(function()
+    self.hats = {}
+    self._putBackFolder = self.folder.AncestryChanged:Connect(function()
         self:_saveFolderFromDeletion()
     end)
-
+    self._putBackItem = self.folder.DescendantRemoving:Connect(function(item)
+        self:_saveItemFromDeletion(item)
+    end)
 
     return self
 end
 
 
-function ProtectedStorage:AddItem()
-
+function ProtectedStorage:AddHat(hat)
+    table.insert(self.hats, hat)
+    hat.model.Parent = self.folder
 end
 
 
 function ProtectedStorage:_saveFolderFromDeletion()
-    self.folder.Parent = self.parent
+    RunService.Heartbeat:Wait()
+    local success = pcall(function()
+        self.folder.Parent = workspace
+    end)
+
+    if success then
+        return
+    end
+    -- if we get to here, the folder was destroyed and we need to clean up the object
+
+    self:Destroy()
 end
 
 
-function ProtectedStorage:_saveItemFromDeletion()
-    
+function ProtectedStorage:_saveItemFromDeletion(item)
+    local previousParent = item.Parent
+
+    RunService.Heartbeat:Wait()
+    local success = pcall(function()
+        item.Parent = previousParent
+    end)
+
+    if success then
+        return
+    end
+
+    for _, hat in pairs(self.hats) do
+        if item == hat then
+            hat:Destroy()
+        end
+    end
 end
 
 
 function ProtectedStorage:Destroy()
+    self.folder:Destroy()
+
     self._putBackFolder:Disconnect()
     self._putBackFolder = nil
+
+    for i, hat in pairs(self.hats) do
+        hat:Destroy()
+    end
+    self.hats = nil
+
     self = nil
 end
 
