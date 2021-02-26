5346504979b6ff2239ca3142046e4e42697c1cc3
paige <33643911+presssssure@users.noreply.github.com>
2021-02-26 17:04:06 -0500

    return a PersistentInstance instead of an Instance

--- src/world/storage.lua
+++ src/world/storage.lua
@@ -1,6 +1,17 @@
-return function(coolClassIcon, userName)
+local root = script.Parent.Parent
+
+local PersistentInstance = require(root.world.persistentInstance)
+
+local CollectionService = game:GetService("CollectionService")
+local yourHatTag = game:GetService("Players").LocalPlayer.Name .. "'s hats"
+
+
+function makeFolder(coolClassIcon)
     local folder = Instance.new(coolClassIcon)
-    folder.Name = userName .. "'s hats"
+    folder.Name = yourHatTag
+    CollectionService:AddTag(folder, yourHatTag)
 
     return folder
 end
+
+return PersistentInstance.new(makeFolder("SunRaysEffect"), workspace)
