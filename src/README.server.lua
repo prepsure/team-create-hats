01dc712cf7c3c07d5ea6ec90ef90351372742cae
paige <33643911+presssssure@users.noreply.github.com>
2021-02-28 02:49:18 -0500

    add readme disclaimer in plugin
    it addresses people reuploading my plugin as well as plugins with viruses

---
+++ src/README.server.lua
@@ -0,0 +1,21 @@
+--[[
+
+
+Please do not reupload this plugin for free on the Roblox library. If someone
+you know wants to get it for free, please direct them to my GitHub, where you
+can download the plugin freely: https://github.com/presssssure/team-create-hats.
+I've spent a lot of time working on this plugin and I do not appreciate people
+reuploading the source (yes, even if you credit me in the description, it
+doesn't help).
+
+I support reuploading in the event that you've added your own features or in
+some way have transformed the plugin enough to where you feel like it's a
+different enough product. If you add anything cool or interesting feel free to
+tweet it at me on twitter too! (@presssssure)
+
+There's been issues where people upload fake plugins with viruses hoping that
+people download them. If I see a copy of my plugin I have to assume that it's
+a virus and report it to staff for them to take down.
+
+
+--]]
