7035a7b17a430724d960ab8fcda2417322bd199b
paige <33643911+presssssure@users.noreply.github.com>
2021-03-02 16:03:05 -0500

    make functions for commas and fields

--- src/gui/EditorWindow/Vector3Input.lua
+++ src/gui/EditorWindow/Vector3Input.lua
@@ -7,8 +7,58 @@ local NumberInput = require(script.Parent.NumberInput)
 local Vector3Input = Roact.Component:extend("Vector3Input")
 
 
+local function getVectorBaseFromDirection(dir)
+    dir = dir:lower()
+    return (dir == 'x') and Vector3.new(1, 0, 0) or
+           (dir == 'y') and Vector3.new(0, 1, 0) or
+           (dir == 'z') and Vector3.new(0, 0, 1)
+end
+
+
 function Vector3Input:updateValue()
     self.props.callback(self.props.DefaultValue)
+end
+
+
+-- dir must be X, Y, or Z
+function Vector3Input:makeField(dir)
+    local vectorBase = getVectorBaseFromDirection(dir)
+
+    return Roact.createElement(NumberInput, {
+        Position = UDim2.new(0.3, 0, 0, 0),
+        Size = UDim2.new(0.2, 0, 1, 0),
+
+        NumType = "all",
+        Theme = self.props.Theme,
+        DefaultValue = self.props.DefaultValue[dir],
+
+        callback = function(num)
+            self.props.DefaultValue = num * vectorBase + (self.props.DefaultValue * (Vector3.new(1, 1, 1) - vectorBase))
+            self:updateValue()
+        end,
+    })
+end
+
+
+function Vector3Input:makeComma()
+    return Roact.createElement("TextLabel", {
+
+        Position = UDim2.new(0.775, 0, 0, 0),
+        Size = UDim2.new(0.05, 0, 1, 0),
+        Text = ",",
+        BackgroundTransparency = 1,
+        TextColor3 = self.props.Theme:GetColor(
+            Enum.StudioStyleGuideColor.MainText,
+            Enum.StudioStyleGuideModifier.Default
+        ),
+        TextScaled = true,
+        TextXAlignment = Enum.TextXAlignment.Left,
+
+    }, {
+        TSize = Roact.createElement("UITextSizeConstraint", {
+            MaxTextSize = 12,
+        }),
+    })
 end
 
 
@@ -53,85 +103,11 @@ function Vector3Input:render()
                 }),
             }),
 
-            X = Roact.createElement(NumberInput, {
-                Position = UDim2.new(0.3, 0, 0, 0),
-                Size = UDim2.new(0.2, 0, 1, 0),
-
-                NumType = "all",
-                Theme = self.props.Theme,
-                DefaultValue = self.props.DefaultValue.X,
-
-                callback = function(num)
-                    self.props.DefaultValue = Vector3.new(num, 0, 0) + (self.props.DefaultValue * Vector3.new(0, 1, 1))
-                    self:updateValue()
-                end,
-            }),
-
-            Comma1 = Roact.createElement("TextLabel", {
-
-                Position = UDim2.new(0.525, 0, 0, 0),
-                Size = UDim2.new(0.05, 0, 1, 0),
-                Text = ",",
-                BackgroundTransparency = 1,
-                TextColor3 = self.props.Theme:GetColor(
-                    Enum.StudioStyleGuideColor.MainText,
-                    Enum.StudioStyleGuideModifier.Default
-                ),
-                TextScaled = true,
-                TextXAlignment = Enum.TextXAlignment.Left,
-
-            }, {
-                TSize = Roact.createElement("UITextSizeConstraint", {
-                    MaxTextSize = 12,
-                }),
-            }),
-
-            Y = Roact.createElement(NumberInput, {
-                Position = UDim2.new(0.55, 0, 0, 0),
-                Size = UDim2.new(0.2, 0, 1, 0),
-
-                NumType = "all",
-                Theme = self.props.Theme,
-                DefaultValue = self.props.DefaultValue.Y,
-
-                callback = function(num)
-                    self.props.DefaultValue = Vector3.new(0, num, 0) + (self.props.DefaultValue * Vector3.new(1, 0, 1))
-                    self:updateValue()
-                end,
-            }),
-
-            Comma2 = Roact.createElement("TextLabel", {
-
-                Position = UDim2.new(0.775, 0, 0, 0),
-                Size = UDim2.new(0.05, 0, 1, 0),
-                Text = ",",
-                BackgroundTransparency = 1,
-                TextColor3 = self.props.Theme:GetColor(
-                    Enum.StudioStyleGuideColor.MainText,
-                    Enum.StudioStyleGuideModifier.Default
-                ),
-                TextScaled = true,
-                TextXAlignment = Enum.TextXAlignment.Left,
-
-            }, {
-                TSize = Roact.createElement("UITextSizeConstraint", {
-                    MaxTextSize = 12,
-                }),
-            }),
-
-            Z = Roact.createElement(NumberInput, {
-                Position = UDim2.new(0.8, 0, 0, 0),
-                Size = UDim2.new(0.2, 0, 1, 0),
-
-                NumType = "all",
-                Theme = self.props.Theme,
-                DefaultValue = self.props.DefaultValue.Z,
-
-                callback = function(num)
-                    self.props.DefaultValue = Vector3.new(0, 0, num) + (self.props.DefaultValue * Vector3.new(1, 1, 0))
-                    self:updateValue()
-                end,
-            }),
+            X = Vector3Input:makeField("X"),
+            Comma1 = Vector3Input:makeComma(),
+            Y = Vector3Input:makeField("Y"),
+            Comma2 = Vector3Input:makeComma(),
+            Z = Vector3Input:makeField("Z")
 
         })
     })
