d0093ca14aea76a054b48a681cb6fe12a8e3376e
Paige <33643911+prepsure@users.noreply.github.com>
2025-11-16 01:43:17 -0800

    init new tools

--- src/README.server.lua
+++ src/README.server.lua
@@ -1,17 +1,14 @@
 --[[
 
 
-Please do not reupload this plugin for free on the Roblox library. If someone
+Please do not reupload this plugin on the Roblox Creator Store. If someone
 you know wants to get it for free, please direct them to my GitHub, where you
-can download the plugin freely: https://github.com/presssssure/team-create-hats.
-I've spent a lot of time working on this plugin and I do not appreciate people
-reuploading the source (yes, even if you credit me in the description, it
-doesn't help).
+can download the plugin freely: https://github.com/prepsure/team-create-hats.
 
 I support reuploading in the event that you've added your own features or in
 some way have transformed the plugin enough to where you feel like it's a
 different enough product. If you add anything cool or interesting feel free to
-tweet it at me on twitter too! (@presssssure)
+show it to me too!
 
 There's been issues where people upload fake plugins with viruses hoping that
 people download them. If I see a copy of my plugin I have to assume that it's
